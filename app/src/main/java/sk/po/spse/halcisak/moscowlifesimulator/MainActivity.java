108ad1b2f73cdc60aa0aac06a31705c71bf5393b
matej <matej.koscelnik@gmail.com>
2021-03-24 18:50:38 +0100

    kod1.2

--- app/src/main/java/sk/po/spse/halcisak/moscowlifesimulator/MainActivity.java
+++ app/src/main/java/sk/po/spse/halcisak/moscowlifesimulator/MainActivity.java
@@ -34,13 +34,13 @@ public class MainActivity extends AppCompatActivity {
 
         private FrameLayout pozadie;
         private ImageView opica;
-        private ImageView budova;
+        private ImageView budova,budova2;
         private TextView HscoreView;
         private TextView ScoreView;
         private TextView play;
 
-        private float budovaX, budovaY, opicaY;
-        private boolean skacem, idemhore;
+        private float budovaX, budovaY, opicaY, budova2x, budova2y;
+        private boolean idemhore;
         private int perioda = 10;
 
         private int hScore;
@@ -59,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
             pozadie = findViewById(R.id.pozadie);
             opica = findViewById(R.id.spiderman);
             budova = findViewById(R.id.auto);
+            budova2 = findViewById(R.id.auto2);
             HscoreView = findViewById(R.id.highScoreView);
             ScoreView = findViewById(R.id.scoreView);
             play = findViewById(R.id.play);
@@ -83,28 +84,33 @@ public class MainActivity extends AppCompatActivity {
             play.setVisibility(View.VISIBLE);
             opica.setVisibility(View.GONE);
             budova.setVisibility(View.GONE);
-
+            budova2.setVisibility(View.GONE);
            // HscoreView.setText("High score : "+hScore);
             HscoreView.setText("High score : "+opica.getY());
 
 
 
-            budovaX = opica.getX()+50;
+            budovaX = opica.getX()+800;
             budova.setX(budovaX);
             budovaY =(int)((displayMetrics.heightPixels/2)+(displayMetrics.heightPixels/8));
             budova.setY(budovaY);
+            budova2x=budovaX+1200;
+            budova2y=budovaY;
+            budova2.setY(budova2y);
+            budova2.setX(budova2x);
 
         }
         public void start(View view){
           //  opica.setImageResource(R.drawable.neskace);
             score = 0;
             ScoreView.setText(score+" : Score");
-            skacem=false;
+
             opicaY = 0;
             opica.setY(opicaY);
             play.setVisibility(View.GONE);
             opica.setVisibility(View.VISIBLE);
             budova.setVisibility(View.VISIBLE);
+            budova2.setVisibility(View.VISIBLE);
             prehra=false;
             timerB = new Timer();
             timerB.schedule(new TimerTask() {
@@ -114,7 +120,8 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void run() {
                             posunAuto();
-                            if (!idemhore &&  !(opica.getX()+opica.getWidth()>=budovaX && opica.getX()<=budovaX+budova.getWidth() && opica.getY()-opica.getHeight()==budova.getY()-budova.getHeight())) {
+                            posunAuto2();
+                            if (!idemhore &&  !(opica.getX()+opica.getWidth()>=budovaX && opica.getX()<=budovaX+budova.getWidth() && opica.getY()-opica.getHeight()==budova.getY()-budova.getHeight()) && !(opica.getX()+opica.getWidth()>=budova2x && opica.getX()<=budova2x+budova.getWidth() && opica.getY()-opica.getHeight()==budova2.getY()-budova2.getHeight())) {
                                 spadni();
                             }
                             HscoreView.setText("High score : "+opica.getY());
@@ -128,13 +135,13 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
                 }
-            }, 0, 10);
+            }, 0, 1);
             if (!prehra){
                 pozadie.setOnClickListener(new View.OnClickListener() {
 
                     @Override
                     public void onClick(View v) {
-                        if (opica.getX()+opica.getWidth()>=budovaX && opica.getX()<=budovaX+budova.getWidth() && opica.getY()-opica.getHeight()==budova.getY()-budova.getHeight()){
+                        if ((opica.getX()+opica.getWidth()>=budovaX && opica.getX()<=budovaX+budova.getWidth() && opica.getY()-opica.getHeight()==budova.getY()-budova.getHeight())||(opica.getX()+opica.getWidth()>=budova2x && opica.getX()<=budova2x+budova.getWidth() && opica.getY()-opica.getHeight()==budova2.getY()-budova2.getHeight())){
                          //   opica.setImageResource(R.drawable.skace);
 
                             idemhore=true;
@@ -167,7 +174,7 @@ public class MainActivity extends AppCompatActivity {
 
                                 }
 
-                            }, 0, 10);
+                            }, 0, 7);
                         }
                     }
                 });
@@ -176,19 +183,33 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public void posunAuto(){
-            if (budovaX < -budova.getWidth()*2){
-                budovaX =displayMetrics.widthPixels;
-                budova.setX(budovaX);
-                score++;
-           //     mediaPlayer = MediaPlayer.create(this ,R.raw.coin_effect);
- //               mediaPlayer.start();
-                ScoreView.setText(score+" : Score");
-            }
-
-            budovaX -= 2;
+        if (budovaX < -budova.getWidth()*2){
+            budovaX =displayMetrics.widthPixels;
             budova.setX(budovaX);
-
+            score++;
+            //     mediaPlayer = MediaPlayer.create(this ,R.raw.coin_effect);
+            //               mediaPlayer.start();
+            ScoreView.setText(score+" : Score");
         }
+
+        budovaX -= 2;
+        budova.setX(budovaX);
+
+    }
+    public void posunAuto2(){
+        if (budova2x < -budova2.getWidth()*2){
+            budova2x =displayMetrics.widthPixels;
+            budova2.setX(budova2x);
+            score++;
+            //     mediaPlayer = MediaPlayer.create(this ,R.raw.coin_effect);
+            //               mediaPlayer.start();
+            ScoreView.setText(score+" : Score");
+        }
+
+        budova2x -= 2;
+        budova2.setX(budova2x);
+
+    }
         //450
         public void vyskoc(){
             //mediaPlayer = MediaPlayer.create(this ,R.raw.jump_effect);
@@ -196,16 +217,16 @@ public class MainActivity extends AppCompatActivity {
             if (idemhore){
                 opicaY -=4;
                 opica.setY(opicaY);
-                if (opicaY < 50){
+                if (opicaY < 100){
                     idemhore=false;
                     timerO.cancel();
-                    skacem=false;
+
                 }
             }
 
         }
         public void spadni(){
-            opicaY +=2;
+            opicaY +=1;
             opica.setY(opicaY);
 
 
